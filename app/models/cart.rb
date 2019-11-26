c2e3ed7429a65fcae92948d2cec73e5957fb85f9
Martin Hynes <martinhynesone@gmail.com>
2019-11-26 06:26:06 +0000

    Added quantities

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,13 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
+  
+  def add_tutor(tutor)
+    current_item = line_items.find_by(tutor_id: tutor.id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(tutor_id: tutor.id)
+    end
+    current_item
+  end
 end
